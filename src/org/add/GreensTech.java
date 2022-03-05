f5fa8c35df19030355352bd75704df5cca66660c
unknown <afir4u@gmail.com>
2022-03-05 10:53:49 +0530

    project created

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,35 @@
+package org.add;
+
+public class GreensTech {
+
+	   public void greAd() {
+		System.out.println("Greens Adayar");
+		
+ }
+	   public void greOmr() {
+		System.out.println("Greens Omr");
+	}
+	
+	       public void gretm() {
+		System.out.println("Greens Tambaram");
+		
+	}
+	
+	     public void greVl() {
+		System.out.println("Greens Velacherry");
+	}
+	
+	     public void greAn() {
+		System.out.println("Greens Anna nagar");
+	}
+	
+	public static void main(String[] args) {
+		 GreensTech e=new GreensTech();
+		 e.greAd();
+		 e.greAn();
+		 e.greOmr();
+		 e.gretm();
+		 e.greVl();
+	}
+		
+}
